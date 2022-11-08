47afe721a497c802fec60ae4bcd2cbbce98e3a5c
Angel Gutierrez <anggutie.dev@gmail.com>
2022-11-07 22:40:57 -0600

    update dag kubernetesoperator

--- dags/titanic_k8s_operator.py
+++ dags/titanic_k8s_operator.py
@@ -33,13 +33,13 @@ create_preprocessing = KubernetesPodOperator(dag=dag,
     task_id='create_preprocessing',
     cmds=["python3", "-c"],
     arguments=["from pipelines.titanic import create_preprocessing_pipeline; create_preprocessing_pipeline('submodules/classification_projects/titanic_challenge/data/train.csv', True)"],
-    executor_config = resource_config
+    executor_config = resource_config,
     namespace="default",
     image="ghcr.io/anggutie-dev/airflow-custom:latest",
     name="create-preprocessing-pod",
     in_cluster=True,
     is_delete_operator_pod=True,
-    get_logs=True,
+    get_logs=True
 )
 
 create_preprocessing
